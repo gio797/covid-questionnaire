1cf3f63c99b7c688f94ed9aa67d3611b7c616af2
gio797 <119562363+gio797@users.noreply.github.com>
2023-06-30 15:29:48 +0400

    modify commo.css and covid files

--- pages/Covid/covid.js
+++ pages/Covid/covid.js
@@ -2,6 +2,13 @@ const covid_yes = document.querySelector("#covid_yes");
 const covid_no = document.querySelector("#covid_no");
 const covid_now = document.querySelector("#covid_now");
 
+const test_yes = document.querySelector("#test_yes");
+const test_no = document.querySelector("#test_no");
+
+const test_amount_title = document.querySelector(".test-amount-title");
+const test_date = document.querySelector("#test_date");
+const amount = document.querySelector("#amount");
+
 const test_question = document.querySelector(".test-question");
 const test_boxes = document.querySelectorAll(".test-box");
 
@@ -18,4 +25,10 @@ covid_no.addEventListener("input", () => {
 covid_now.addEventListener("input", () => {
   test_question.classList.remove("active");
   test_boxes.forEach((box) => box.classList.remove("active"));
+});
+
+test_yes.addEventListener("input", () => {
+  test_amount_title.classList.add("active");
+  test_date.classList.add("active");
+  amount.classList.add("active");
 });
