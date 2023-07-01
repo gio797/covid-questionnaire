77a553d4e7bd35d6e81d3e7bef9cf917c9978473
gio797 <119562363+gio797@users.noreply.github.com>
2023-07-01 11:24:14 +0400

    add vaccine files

---
+++ pages/Vaccine/vaccine.js
@@ -0,0 +1,30 @@
+const vaccine_yes = document.querySelector("#vaccine_yes");
+const vaccine_no = document.querySelector("#vaccine_no");
+
+const level_question = document.querySelector(".level-question");
+const level_boxes = document.querySelectorAll(".level-box");
+
+const wait_question = document.querySelector(".wait-question");
+const wait_boxes = document.querySelectorAll(".wait-box");
+
+vaccine_yes.addEventListener("input", () => {
+  level_question.classList.add("active");
+  level_boxes.forEach((box) => box.classList.add("active"));
+  wait_question.classList.remove("active");
+  wait_boxes.forEach((box) => box.classList.remove("active"));
+});
+
+vaccine_no.addEventListener("input", () => {
+  wait_question.classList.add("active");
+  wait_boxes.forEach((box) => box.classList.add("active"));
+  level_question.classList.remove("active");
+  level_boxes.forEach((box) => box.classList.remove("active"));
+});
+
+function onPreviousPage() {
+  document.location.href = "../Covid/covid.html";
+}
+
+function onNextPage() {
+  document.location.href = "../Office/office.html";
+}
